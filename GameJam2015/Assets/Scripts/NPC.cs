7e75c29c58b3ba28ef383b81af0542bbbaf3aa47
SVT125 <Jhardjadinata14@gmail.com>
2015-04-11 11:57:09 -0700

    added player class, separated methods from derived npc

--- GameJam2015/Assets/Scripts/NPC.cs
+++ GameJam2015/Assets/Scripts/NPC.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 //This is meant to be an abstract class.
 public class NPC : MonoBehaviour {
-	private System.Random r;
+	protected System.Random r;
 	protected bool isIdle;
 	protected int damage;
 
@@ -12,25 +12,6 @@ public class NPC : MonoBehaviour {
 	void Start () {
 		r = new System.Random ();
 		isIdle = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	public virtual void converse() { }
-
-	void OnTriggerEnter(Collider coll) {
-		if(coll.tag == "NPC")
-			if (isIdle) {
-				isIdle = false;
-				converse ();
-			}
-	}
-
-	void OnTriggerExit(Collider coll) {
-		//just in case...
 	}
 }
 
--- GameJam2015/Assets/Scripts/TrollNPC.cs
+++ GameJam2015/Assets/Scripts/TrollNPC.cs
@@ -3,28 +3,16 @@ using System;
 using System.Collections;
 
 public class TrollNPC : NPC {
-	private String[] greetings = {
-		"Good morning scrub",
-		"Hello skrob"
-	}, goodbyes = {
-		"cya nub",
-		"get rekt"
-	};
+	private string[] goodbyes;
+	private AudioClip[] dialog;
+
+	public string[] getGoodbyes() { return this.goodbyes; }
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	public override void converse() {
-		System.Random r = new System.Random ();
-		Debug.Log (greetings[r.Next(0,greetings.Length)]); //placeholder greeting
-		//conversation...
-		Debug.Log (goodbyes [r.Next (0,goodbyes.Length)]); //placeholder goodbye
+		this.goodbyes = new string[] {
+			"cya nub",
+			"get rekt"
+		};
 	}
 }
