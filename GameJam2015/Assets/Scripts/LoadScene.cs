f380e4084c42c4b76f2727d96eb29e78dd72f074
Natalie <natalie42@me.com>
2015-04-10 22:47:06 -0700

    edited menu scene

---
+++ GameJam2015/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public void Testing() {
+		Application.LoadLevel ("other scene");
+	}
+
+	public void Menu() {
+		Application.LoadLevel ("menu test");
+	}
+}
---
+++ GameJam2015/Assets/Scripts/view.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class view : MonoBehaviour {
+
+	public Canvas canvas;
+
+	void Start () {
+		canvas = gameObject.GetComponent ("Canvas") as Canvas;
+		gameObject.SetActive(false);
+	}
+
+}
