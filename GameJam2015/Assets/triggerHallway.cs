b3c636a4df7bf810bb75544f38b4ddaa61c7d248
SeanCrev <seandcreveling@gmail.com>
2015-04-12 00:08:57 -0700

    Editor edits. Cylinder trap

---
+++ GameJam2015/Assets/triggerHallway.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerHallway : MonoBehaviour {
+
+    GameObject manager;
+
+	// Use this for initialization
+	void Start () 
+    {
+        manager = GameObject.Find("MainCylinder");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider x)
+    {
+        if(x.tag == "Player")
+        {
+            manager.GetComponent<circleTrap>().changeHallway();
+
+        }
+    }
+}
