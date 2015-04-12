a2c3c17a69c8e7c081bf9e74ba7ae464cfedf1ba
zach-anderson <zanderso@uci.edu>
2015-04-12 15:00:00 -0700

    Collision Detect
    whooooooo

---
+++ GameJam2015/Assets/collisionDetectAdvance.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionDetectAdvance : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.root.CompareTag("Player"))
+		{
+			Application.LoadLevel("trash title");
+		}
+	}
+
+
+}
