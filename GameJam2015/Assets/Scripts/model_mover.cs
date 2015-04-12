3b07e629ec581924450ff8936ecff16f1bbef22e
warstark <smacha@uci.edu>
2015-04-11 17:03:21 -0700

    changed move script

--- GameJam2015/Assets/Scripts/model_mover.cs
+++ GameJam2015/Assets/Scripts/model_mover.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
-
+//hello
 public class model_mover : MonoBehaviour {
 	public Rigidbody rb;
 	public Rigidbody rb2;
@@ -15,31 +15,29 @@ public class model_mover : MonoBehaviour {
 	public float speed; 
 	public bool canjump=false;
 	public int jumpcount=0;
+	public int stopper = 0;
 	public float jumpheight;
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
 		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical = Input.GetAxis ("Vertical");
-
+		
 		Vector3 jumper = new Vector3 (0.0f, jumpforce, 0.0f);
-		Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
-
-		if (vertical == 0) 
-		{
-			Vector3 zero = new Vector3(0,0,0);
-			movement = zero;
-		}
-
-		rb.AddForce (movement*speed);
-
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * 90 * Time.deltaTime, 0);
+		Vector3 movement = new Vector3 (0.0f, 0.0f, vertical);
+		rb.AddForce (movement * speed*Time.deltaTime);
 		
 		if (Input.GetButtonDown ("Jump") && jumpcount == 0) 
 		{
 			rb2.velocity=jumper;
 			jumpcount=1;
 		}
-		
+		if ((Input.GetButtonDown("Horizontal")||Input.GetButtonDown("Vertical"))&&Input.GetButtonDown ("Jump")) 
+		{
+			rb2.velocity=jumper;
+			jumpcount=1;
+		}
 		if (rb2.velocity.y == 0) 
 		{
 			jumpcount=0;
