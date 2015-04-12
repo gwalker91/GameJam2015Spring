9352050f5234c1a7eb1000f35e04cc12b7335c7a
Natalie <natalie42@me.com>
2015-04-12 12:10:13 -0700

    teleportation forever

---
+++ GameJam2015/Assets/teleporterScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleporterScript : MonoBehaviour {
+
+	public Collider teleporter;
+	public Transform player;
+	GameObject[] locations;
+
+	void Start() {
+		locations = GameObject.FindGameObjectsWithTag ("Location");
+	}
+
+	void OnTriggerEnter(Collider teleporter){
+		Vector3 position = player.position;
+		Vector3 new_position = locations [Random.Range (0, locations.Length)].transform.position;
+		transform.position = new_position;
+	}
+}
