65f3fa714aeacd446b72d3c775b56d7118b2ce22
Grant <gwwalker@uci.edu>
2015-04-12 11:17:48 -0700

    Added a prefab for narrator points and an audiomanager Prefabs/AudioManager.prefab

--- GameJam2015/Assets/Scripts/KillOnFalling.cs
+++ GameJam2015/Assets/Scripts/KillOnFalling.cs
@@ -17,6 +17,7 @@ public class KillOnFalling : MonoBehaviour
 		if (transform.position.y < -10.0f) 
 		{
 			transform.position = playerInitPos;
+			GameObject.Find("AudioManager").GetComponent<AudioManager>().Changeclip("Respawn", true);
 		}
 	}
 }
