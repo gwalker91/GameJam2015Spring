a8d1d4f3171a7fc9f0e4f8c6242f6cfd459bbcef
SVT125 <Jhardjadinata14@gmail.com>
2015-04-12 11:00:54 -0700

    added trash miles

---
+++ GameJam2015/Assets/Scripts/NPCs/MilesNPC.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MilesNPC : NPC {
+
+	// Use this for initialization
+	void Start () {
+		this.dict.Add ("You are trash.", new string[] {});
+		this.dict.Add ("Everyone around me is trash.", new string[] {});
+		this.dict.Add ("Everything around me is trash.", new string[] {});
+	}
+}
