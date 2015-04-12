55205912302aa3db37723078a5c95deb9df58601
warstark <smacha@uci.edu>
2015-04-12 15:41:26 -0700

    New Scene and script

---
+++ GameJam2015/Assets/Scripts/Trash_trickster.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trash_trickster : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+
+
+	}*/
+	public Transform target;
+	public Transform target2;
+	public float speed;
+	public float hitcounter=0;
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		//obstacle.transform.Translate (Vector3.forward * Time.deltaTime);
+		float step = speed * Time.deltaTime;
+		if (hitcounter <=50 ) {
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+
+		}
+		if (hitcounter >50) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, target2.position, step);
+
+		}
+		if (hitcounter > 100) {
+			hitcounter=0;
+		}
+		hitcounter++;
+	}
+}
