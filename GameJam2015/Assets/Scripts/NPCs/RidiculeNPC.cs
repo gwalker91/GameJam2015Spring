7d6a78636c5108d5b97c638d05ee7080fd8efcbd
SVT125 <Jhardjadinata14@gmail.com>
2015-04-12 11:56:16 -0700

    more ridicule NPC lines

--- GameJam2015/Assets/Scripts/NPCs/RidiculeNPC.cs
+++ GameJam2015/Assets/Scripts/NPCs/RidiculeNPC.cs
@@ -14,5 +14,9 @@ public class RidiculeNPC : NPC {
 		this.dict.Add ("You suck. Just stop.", new string[] {});
 		this.dict.Add ("No really. Just turn around right now and you won't get rekt.", new string[] {});
 		this.dict.Add ("it is gr8 4 me to appreci8 how rekt u will g8 8/8 i r8", new string[] {});
+		this.dict.Add ("Go ahead, just turn around.", new string[] {});
+		this.dict.Add ("You would accomplish more by sitting on your toilet.", new string[] {});
+		this.dict.Add ("Why would you abandon the sink-toilet?", new string[] {});
+		this.dict.Add ("No. Just go back.", new string[] {});
 	}
 }
